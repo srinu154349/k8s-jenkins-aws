5dbc873b905ea498d7967d273587b0f90468826c
srinu154349 <82919467+srinu154349@users.noreply.github.com>
2022-04-25 15:58:28 +0530

    Update and rename JhooqDockerDemoController.java to srinuDockerDemoController.java

--- src/main/java/com/jhooq/Jhooqk8s/ws/srinuDockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/ws/srinuDockerDemoController.java
@@ -1,13 +1,13 @@
-package com.jhooq.Jhooqk8s.ws;
+package com.srinu.srinuk8s.ws;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class JhooqDockerDemoController {
+public class srinuDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello - Jhooq-k8s i Have updated the message";
+        return "Hello - srinu-k8s i Have updated the message";
     }
 }
